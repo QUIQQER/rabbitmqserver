0f418baa4ffabad7675a1a055d3a6aa241adfff5
Patrick Müller <p.mueller@pcsg.de>
2016-11-11 15:31:22 +0100

    feat: Konsolen-Tool

--- src/QUI/RabbitMQServer/Console.php
+++ src/QUI/RabbitMQServer/Console.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * This file contains \QUI\Translator\Console
+ * This file contains \QUI\RabbitMQServer\Console
  */
 
 namespace QUI\RabbitMQServer;
@@ -9,9 +9,9 @@ namespace QUI\RabbitMQServer;
 use QUI;
 
 /**
- * Creat translation via console
+ * Different console options for RabbitMQ module
  *
- * @author www.pcsg.de (Henning Leutz)
+ * @author Patrick Müller (www.pcsg.de)
  */
 class Console extends QUI\System\Console\Tool
 {
@@ -21,11 +21,11 @@ class Console extends QUI\System\Console\Tool
     public function __construct()
     {
         $this->setName('quiqqer:rabbitmqserver')
-            ->setDescription('Start/Stop/Status for RabbitMQ Consumer.php');
+            ->setDescription('Operations for RabbitMQ QUIQQER module');
 
         $this->addArgument(
             'action',
-            'Action to execute: start, stop, status'
+            'Action to execute: connectiontest'
         );
     }
 
@@ -37,211 +37,30 @@ class Console extends QUI\System\Console\Tool
     public function execute()
     {
         switch ($this->getArgument('action')) {
-            case 'start':
-                if ($this->startConsumer()) {
-                    $this->writeLn(
-                        'php process with RabbitConsumer.php successfully started (pid: '
-                        . $this->getPid() . ').'
-                    );
-                } else {
-                    $this->writeLn(
-                        'RabbitConsumer.php could not be started.'
-                    );
-                }
-                break;
-
-            case 'stop':
-                if ($this->stopConsumer()) {
-                    $this->writeLn(
-                        'Stopping successful.'
-                    );
-                } else {
-                    $this->writeLn(
-                        'Stopping not successful.'
-                    );
-                }
-                break;
-
-            case 'status':
-                if ($this->isConsumerRunning()) {
-                    $this->writeLn(
-                        'RabbitConsumer.php process is running (pid: ' . $this->getPid() . ').'
-                    );
-                } else {
-                    $this->writeLn(
-                        'RabbitConsumer.php process is currently not running.'
-                    );
-                }
+            case 'connectiontest':
+                $this->connectionTest();
                 break;
 
             default:
-                $this->writeLn('Unknown action. Please only use start, stop or status');
+                $this->writeLn('Unknown action. Please only use: connectiontest');
         }
 
         $this->writeLn("\nFinished.\n");
     }
 
-    /**
-     * Starts RabbitConsumer.php process
-     *
-     * @return bool - success
-     */
-    protected function startConsumer()
+    protected function connectionTest()
     {
-        if ($this->isConsumerRunning()) {
-            $this->writeLn(
-                'Cannot start RabbitConsumer.php. Process is already running (pid: '
-                . $this->getPid() . ').'
-            );
+        $this->writeLn(
+            "Testing connection to RabbitMQ server with current settings..."
+        );
 
-            return false;
+        try {
+            Server::getChannel();
+        } catch (\Exception $Exception) {
+            $this->write(" [error] :: " . $Exception->getMessage());
+            return;
         }
 
-        $pipes = array();
-
-//        $process = proc_open(
-//            'php -d display_errors=stderr '
-//            . dirname(dirname(dirname(__FILE__)))
-//            . '/RabbitConsumer.php',
-//            array(),
-//            $pipes
-//        );
-
-        if (!is_resource($process) || !$process) {
-            $this->writeLn(
-                'Something went wrong while executing the RabbitConsumer.php process.'
-            );
-
-            return false;
-        }
-
-        $processInfo = proc_get_status($process);
-
-        if (!$processInfo
-            || !isset($processInfo['pid'])
-            || empty($processInfo['pid'])
-        ) {
-            $this->writeLn(
-                'Could not receive information on RabbitConsumer.php process.'
-            );
-
-            return false;
-        }
-
-        $this->writePidToPidFile($processInfo['pid']);
-
-        return $this->isConsumerRunning();
-    }
-
-    /**
-     * Stops RabbitConsumer.php process
-     *
-     * @return bool - success
-     */
-    protected function stopConsumer()
-    {
-        if (!$this->isConsumerRunning()) {
-            $this->writeLn(
-                'RabbitConsumer.php not running. No stopping necessary.'
-            );
-
-            return true;
-        }
-
-        $pid = $this->getPid();
-
-        // sending kill signal
-        $killed = posix_kill($pid, SIGKILL);
-
-        if (!$killed) {
-            $this->writeLn(
-                'Something went wrong while sending the kill signal. :('
-            );
-
-            return false;
-        }
-
-        if ($this->isConsumerRunning()) {
-            $this->writeLn(
-                'Kill signal was sent, but process seems to be running regardless.'
-                . ' Please check and/or kill manually (pid: ' . $pid . ')'
-            );
-
-            return false;
-        }
-
-        $pidFile = $this->getPidFilePath();
-
-        if (file_exists($pidFile)) {
-            unlink($pidFile);
-        }
-
-        return true;
-    }
-
-    /**
-     * Checks if the RabbitConsumer.php process is currently running
-     *
-     * @return bool
-     */
-    protected function isConsumerRunning()
-    {
-        $pid = $this->getPid();
-
-        if (!$pid) {
-            return false;
-        }
-
-        $output       = array();
-        $returnStatus = 0;
-
-        exec('ps -p ' . $pid, $output, $returnStatus);
-
-        return $returnStatus === 0;
-    }
-
-    /**
-     * Write new RabbitConsumer.php pid to pid file
-     *
-     * @param integer $pid - RabbitConsumer.php process id
-     * @return void
-     */
-    protected function writePidToPidFile($pid)
-    {
-        $pidFile = $this->getPidFilePath();
-        file_put_contents($pidFile, $pid);
-    }
-
-    /**
-     * Return pid of RabbitConsumer.php process or false, if no pid found
-     *
-     * @return false|int
-     */
-    protected function getPid()
-    {
-        $pidFile = $this->getPidFilePath();
-
-        if (!file_exists($pidFile)) {
-            return false;
-        }
-
-        $pid = file_get_contents($pidFile);
-
-        if (empty($pid)) {
-            return false;
-        }
-
-        return (int)trim($pid);
-    }
-
-    /**
-     * Get path of file where RabbitConsumer.php PID is saved
-     *
-     * @return string
-     */
-    protected function getPidFilePath()
-    {
-        $varDir = QUI::getPackage('quiqqer/rabbitmqserver')->getVarDir();
-        return $varDir . 'RabbitConsumer.pid';
+        $this->write(" [ok]");
     }
 }
